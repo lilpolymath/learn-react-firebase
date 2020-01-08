e24bf7d7de970d0357ab5e02131e9cac4799d47f
lilpolymath <ayofavour7@gmail.com>
2020-01-08 12:51:25 +0100

    feat: Setup React router, routes and routing.

---
+++ src/components/App/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Navigation from '../Navigation';
+import AccountPage from '../Account';
+import LandingPage from '../Landing';
+import SignInPage from '../SignIn';
+import AdminPage from '../Admin';
+import HomePage from '../Home';
+import PasswordForgetPage from '../PasswordForget';
+import SignUpPage from '../SignUp';
+import * as ROUTES from '../../constants/routes';
+
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navigation />
+          <hr />
+          <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+          <Route path={ROUTES.HOME} component={HomePage} />
+          <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+          <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+          <Route exact path={ROUTES.LANDING} component={LandingPage} />
+          <Route path={ROUTES.ADMIN} component={AdminPage} />
+          <Route path={ROUTES.PASSWORD_FORGET} component={PasswordForgetPage} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
