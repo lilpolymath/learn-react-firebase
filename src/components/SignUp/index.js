10ca3041176b8cc1d8af1c964b3eb779bc0542fe
lilpolymath <ayofavour7@gmail.com>
2020-01-09 07:04:50 +0100

    fix: bug issue with user creation.

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import * as ROUTES from '../../constants/routes';
 import { Link, withRouter } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
 import { withFirebase } from '../Firebase';
 import { compose } from 'recompose';
 
@@ -26,15 +27,19 @@ class SignUpFormBase extends Component {
   }
 
   onFormSubmit = event => {
-    const { username, passwordOne, email } = this.state;
+    const { passwordOne, email } = this.state;
     this.props.firebase
-      .doCreateUserWithUsernameAndPassword(email, passwordOne)
-      .then(authUser => {
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(() => {
         this.setState({ ...INITIAL_STATE });
         this.props.history.push(ROUTES.HOME);
       })
       .catch(error => this.setState(error));
     event.preventDefault();
+  };
+
+  checker = () => {
+    this.props.firebase.checker();
   };
 
   onChange = event => {
@@ -63,6 +68,7 @@ class SignUpFormBase extends Component {
           value={email}
           onChange={this.onChange}
           placeholder='Email Address'
+          autoComplete='email'
         />
         <input
           name='passwordOne'
@@ -70,6 +76,7 @@ class SignUpFormBase extends Component {
           value={passwordOne}
           onChange={this.onChange}
           placeholder='Your Password'
+          autoComplete='new-password'
         />
         <input
           name='passwordTwo'
@@ -77,6 +84,7 @@ class SignUpFormBase extends Component {
           value={passwordTwo}
           onChange={this.onChange}
           placeholder='Confirm Password'
+          autoComplete='new-password'
         />
         <button disabled={isInvalid} type='submit'>
           Submit
@@ -89,7 +97,7 @@ class SignUpFormBase extends Component {
 
 const SignUpLink = () => (
   <p>
-    Don't have an account yet? <Link to={ROUTES.SIGN_UP}>Sign up here.</Link>
+    Don't have an account yet? <Link to={ROUTES.SIGN_UP}>Sign up.</Link>
   </p>
 );
 
