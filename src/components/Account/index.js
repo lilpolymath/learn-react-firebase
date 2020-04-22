5cd6966e08c6953648c140d0d92b45ea428d7243
lilpolymath <ayofavour7@gmail.com>
2020-04-22 15:22:12 +0100

    Authorization
    feat: setup basic authorization.

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 
 import { PasswordForgetForm } from '../PasswordForget';
 import PasswordChangeForm from '../PasswordChange';
-import { withAuthorization } from '../Session';
+import { AuthUserContext, withAuthorization } from '../Session';
 
 const AccountPage = () => (
-  <div>
-    <h1>Account</h1>
-    <PasswordChangeForm />
-    <PasswordForgetForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordChangeForm />
+        <PasswordForgetForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
 const condition = authUser => !!authUser;
--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { withAuthorization } from '../Session';
+import * as ROLES from '../../constants/roles';
+
 const Admin = () => (
   <div>
     <h1>Admin</h1>
+    <p>This page is only accessible by adminstrators.</p>
   </div>
 );
 
-export default Admin;
+const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
+
+export default withAuthorization(condition)(Admin);
